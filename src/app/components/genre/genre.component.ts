235affe6cf8d53766680a894b11201cd31d7d454
gustavoszo <g_souza14@outlook.com>
2026-01-20 13:42:41 -0300

    Classes css dinâmicas com NgClass

--- src/app/components/genre/genre.component.ts
+++ src/app/components/genre/genre.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { Genre } from '../book/book';
 import { BookComponent } from '../book/book.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-genre',
-  imports: [BookComponent],
+  imports: [BookComponent, CommonModule],
   templateUrl: './genre.component.html',
   styleUrl: './genre.component.css'
 })
